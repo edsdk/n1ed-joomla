f3a604156400cafb9704c6a13a465f9db182b22c
ak <ak@ak-ASUS>
2019-03-11 22:31:00 +0200

    # Typos and formatting fixed.

--- n1ed.php
+++ n1ed.php
@@ -1,17 +1,19 @@
 <?php
+
 /**
  * N1ED module for Joomla 3
  * Developer: EdSDK
  * Website: https://n1ed.com/
  * License: GNU General Public License Version 2 or later
-**/
+ **/
+
 /**
  * @package     Joomla.Plugin
  * @subpackage  Editors.none
  *
  * @copyright   Copyright (c) 2019 EdSDK (https://n1ed.com/). All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
+ **/
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
@@ -21,197 +23,178 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  1.5
  */
-class plgEditorN1ed extends JPlugin
-{
-  protected $autoloadLanguage = true;
+class plgEditorN1ed extends JPlugin {
 
-	/**
-	 * Method to handle the onInitEditor event.
-	 *  - Initialises the Editor
-	 *
-	 * @return  void
-	 *
-	 * @since 1.5
-	 */
-	public function onInit()
-	{
-    // Check API key
+    protected $autoloadLanguage = true;
 
-    if(!$this->params->get('api_key'))
-    {
+    /**
+     * Method to handle the onInitEditor event.
+     *  - Initialises the Editor
+     *
+     * @return  void
+     *
+     * @since 1.5
+     */
+    public function onInit() {
+        // Check API key
 
-      $plugin_data = $this->getItemByElement('n1ed', 'plugin', 'editors');
+        if (!$this->params->get('api_key')) {
 
-      JFactory::getApplication()->enqueueMessage(JText::sprintf('PLG_N1ED_KEY_WARNING', '/administrator/index.php?option=com_plugins&task=plugin.edit&extension_id=' . $plugin_data['extension_id']), 'warning');
-		}
-		
-    if($this->params->get('api_key'))
-    {
-			// Add N1ED core
+            $plugin_data = $this->getItemByElement('n1ed', 'plugin', 'editors');
 
-			JHtml::_('script', 'https://cdn.n1ed.com/cdn/' . $this->params->get('api_key') . '/n1ed.js?cms=j3', array('version' => 'auto'), array('defer' => 'defer'));
-			
-			JFactory::getDocument()->addStyleDeclaration("
+            JFactory::getApplication()->enqueueMessage(JText::sprintf('PLG_N1ED_KEY_WARNING', '/administrator/index.php?option=com_plugins&task=plugin.edit&extension_id=' . $plugin_data['extension_id']), 'warning');
+        }
 
-				/* Hide editor textarea */
+        if ($this->params->get('api_key')) {
+            // Add N1ED core
 
-				.n1ed-field {
-					display: none !important;
-				}
+            JHtml::_('script', 'https://cdn.n1ed.com/cdn/' . $this->params->get('api_key') . '/n1ed.js?cms=j3', array('version' => 'auto'), array('defer' => 'defer'));
 
-				/* Fix preview modal */
+            JFactory::getDocument()->addStyleDeclaration("
 
-				#sbox-window {
-					z-index: 70000;
-			");
+                /* Hide editor textarea */
+                .n1ed-field {
+                    display: none !important;
+                }
 
-			JFactory::getDocument()->addScriptDeclaration("
+                /* Fix preview modal */
+                #sbox-window {
+                    z-index: 70000;
+                }
+            ");
 
-				(function($, Joomla, window, document){
-					
-					$(document).ready(function(){
+            JFactory::getDocument()->addScriptDeclaration("
 
-						$('.n1ed-field:not(.n1ed-field-processed)').each(function(i) {
-						
-							var editor_id      = $(this).attr('id');
-							var editor_element = $(this).get(0);
-	
-							Joomla.editors.instances[editor_id] = {
-								'id': 			editor_id,
-								'element':  editor_element,
-								'getValue': function ()     { return this.element.value; },
-								'setValue': function (text) { return this.element.value = text; },
-							};
+                (function($, Joomla, window, document){
 
-							$(this).addClass('n1ed-field-processed');
-						});
-					});
+                    $(document).ready(function(){
 
-				}(jQuery, Joomla, window, document));
-			");
-    }
-	}
+                        $('.n1ed-field:not(.n1ed-field-processed)').each(function(i) {
 
-  /**
-	 * Get the editor content.
-	 *
-	 * @param   string  $id  The id of the editor field.
-	 *
-	 * @return  string
-	 *
-	 * @deprecated 4.0 Use directly the returned code
-	 */
-	public function onGetContent($id)
-	{
-		return 'Joomla.editors.instances[' . json_encode($id) . '].getValue();';
-	}
+                            var editor_id      = $(this).attr('id');
+                            var editor_element = $(this).get(0);
 
-	/**
-	 * Set the editor content.
-	 *
-	 * @param   string  $id    The id of the editor field.
-	 * @param   string  $html  The content to set.
-	 *
-	 * @return  string
-	 *
-	 * @deprecated 4.0 Use directly the returned code
-	 */
-	public function onSetContent($id, $html)
-	{
-		return 'Joomla.editors.instances[' . json_encode($id) . '].setValue(' . json_encode($html) . ');';
-	}
+                            Joomla.editors.instances[editor_id] = {
+                                'id': 			editor_id,
+                                'element':  editor_element,
+                                'getValue': function ()     { return this.element.value; },
+                                'setValue': function (text) { return this.element.value = text; },
+                            };
 
-	/**
-	 * Display the editor area.
-	 *
-	 * @param   string   $name     The control name.
-	 * @param   string   $content  The contents of the text area.
-	 * @param   string   $width    The width of the text area (px or %).
-	 * @param   string   $height   The height of the text area (px or %).
-	 * @param   integer  $col      The number of columns for the textarea.
-	 * @param   integer  $row      The number of rows for the textarea.
-	 * @param   boolean  $buttons  True and the editor buttons will be displayed.
-	 * @param   string   $id       An optional ID for the textarea (note: since 1.6). If not supplied the name is used.
-	 * @param   string   $asset    The object asset
-	 * @param   object   $author   The author.
-	 * @param   array    $params   Associative array of editor parameters.
-	 *
-	 * @return  string
-	 */
-	public function onDisplay($name, $content, $width, $height, $col, $row, $buttons = true, $id = null, $asset = null, $author = null, $params = array())
-	{
-		if (empty($id))
-		{
-			$id = $name;
-		}
-        
-		$editor = '
-								<div class="n1ed-field-wrapper">
-									<textarea class="n1ed-field" n1ed="true" name="' . $name . '" id="' . $id . '">' . $content . '</textarea>
-								</div>	
-							';
-    
-    // Add "Preview" button.
+                            $(this).addClass('n1ed-field-processed');
+                        });
+                    });
 
-		// $editor .= '<div class="xtd-buttons-wrapper">'
-		// 							. $this->_displayButtons($name, $buttons, $asset, $author) . 
-		// 						'</div>';
-		
-		return $editor;
-	}
-	
-	/**
-	 * Displays the editor buttons.
-	 *
-	 * @param   string  $name     The control name.
-	 * @param   mixed   $buttons  [array with button objects | boolean true to display buttons]
-	 * @param   string  $asset    The object asset
-	 * @param   object  $author   The author.
-	 *
-	 * @return  void|string HTML
-	 */
-	public function _displayButtons($name, $buttons, $asset, $author)
-	{
-		if (is_array($buttons) || (is_bool($buttons) && $buttons))
-		{
-			$buttons = $this->_subject->getButtons($name, $buttons, $asset, $author);
-
-			return JLayoutHelper::render('joomla.editors.buttons', $buttons);
-		}
-	}
-	
-  /**
-   * Helper for get info about extensions.
-   * 
-   * @param string $element The extension name.
-   * @param string $type    The extension type (plugin, module, component).
-   * @param string $folder  The extension folder (editors, captcha...).
-   */
-  private function getItemByElement($element, $type, $folder = null)
-  {
-    $dbo = JFactory::getDbo();
-    $query = $dbo->getQuery(true);
-    $query->select('extension_id, manifest_cache');
-    $query->from('#__extensions');
-
-    $query->where('element = '.$dbo->quote($element));
-    $query->where('type = '.$dbo->quote($type));
-
-    if($type == 'plugin' && !empty($folder))
-    {
-      $query->where('folder = '.$dbo->quote($folder));
+                }(jQuery, Joomla, window, document));
+            ");
+        }
     }
 
-    $dbo->setQuery($query);
-    $extensionRecord = $dbo->loadAssoc();
-
-    if(!is_null($extensionRecord))
-    {
-      $manifestData = json_decode($extensionRecord['manifest_cache']);
-      $extensionRecord['manifest'] = $manifestData;
+    /**
+     * Get the editor content.
+     *
+     * @param   string  $id  The id of the editor field.
+     *
+     * @return  string
+     *
+     * @deprecated 4.0 Use directly the returned code
+     */
+    public function onGetContent($id) {
+        return 'Joomla.editors.instances[' . json_encode($id) . '].getValue();';
     }
 
-    return $extensionRecord;
-	}
-	
+    /**
+     * Set the editor content.
+     *
+     * @param   string  $id    The id of the editor field.
+     * @param   string  $html  The content to set.
+     *
+     * @return  string
+     *
+     * @deprecated 4.0 Use directly the returned code
+     */
+    public function onSetContent($id, $html) {
+        return 'Joomla.editors.instances[' . json_encode($id) . '].setValue(' . json_encode($html) . ');';
+    }
+
+    /**
+     * Display the editor area.
+     *
+     * @param   string   $name     The control name.
+     * @param   string   $content  The contents of the text area.
+     * @param   string   $width    The width of the text area (px or %).
+     * @param   string   $height   The height of the text area (px or %).
+     * @param   integer  $col      The number of columns for the textarea.
+     * @param   integer  $row      The number of rows for the textarea.
+     * @param   boolean  $buttons  True and the editor buttons will be displayed.
+     * @param   string   $id       An optional ID for the textarea (note: since 1.6). If not supplied the name is used.
+     * @param   string   $asset    The object asset
+     * @param   object   $author   The author.
+     * @param   array    $params   Associative array of editor parameters.
+     *
+     * @return  string
+     */
+    public function onDisplay($name, $content, $width, $height, $col, $row, $buttons = true, $id = null, $asset = null, $author = null, $params = array()) {
+        if (empty($id)) {
+            $id = $name;
+        }
+
+        $editor = '
+            <div class="n1ed-field-wrapper">
+                    <textarea class="n1ed-field" n1ed="true" name="' . $name . '" id="' . $id . '">' . $content . '</textarea>
+            </div>	
+        ';
+
+        return $editor;
+    }
+
+    /**
+     * Displays the editor buttons.
+     *
+     * @param   string  $name     The control name.
+     * @param   mixed   $buttons  [array with button objects | boolean true to display buttons]
+     * @param   string  $asset    The object asset
+     * @param   object  $author   The author.
+     *
+     * @return  void|string HTML
+     */
+    public function _displayButtons($name, $buttons, $asset, $author) {
+        if (is_array($buttons) || (is_bool($buttons) && $buttons)) {
+            $buttons = $this->_subject->getButtons($name, $buttons, $asset, $author);
+
+            return JLayoutHelper::render('joomla.editors.buttons', $buttons);
+        }
+    }
+
+    /**
+     * Helper for get info about extensions.
+     * 
+     * @param string $element The extension name.
+     * @param string $type    The extension type (plugin, module, component).
+     * @param string $folder  The extension folder (editors, captcha...).
+     */
+    private function getItemByElement($element, $type, $folder = null) {
+        $dbo = JFactory::getDbo();
+        $query = $dbo->getQuery(true);
+        $query->select('extension_id, manifest_cache');
+        $query->from('#__extensions');
+
+        $query->where('element = ' . $dbo->quote($element));
+        $query->where('type = ' . $dbo->quote($type));
+
+        if ($type == 'plugin' && !empty($folder)) {
+            $query->where('folder = ' . $dbo->quote($folder));
+        }
+
+        $dbo->setQuery($query);
+        $extensionRecord = $dbo->loadAssoc();
+
+        if (!is_null($extensionRecord)) {
+            $manifestData = json_decode($extensionRecord['manifest_cache']);
+            $extensionRecord['manifest'] = $manifestData;
+        }
+
+        return $extensionRecord;
+    }
+
 }
--- script.n1ed.php
+++ script.n1ed.php
@@ -7,42 +7,40 @@
 **/
 defined('_JEXEC') or die('Restricted access');
 
-class plgEditorsN1edInstallerScript 
-{
-  function postflight($type, $parent) 
-  {
-    // Prepare directory for N1ED files
+class plgEditorsN1edInstallerScript {
 
-    $n1ed_files = JPATH_ROOT . '/images/n1ed';
+    function postflight($type, $parent) {
+        
+        // Prepare directory for N1ED files
+        $n1ed_files = JPATH_ROOT . '/images/n1ed';
 
-    if(!JFolder::exists($n1ed_files)) {
-      JFolder::create($n1ed_files);
+        if (!JFolder::exists($n1ed_files)) {
+            JFolder::create($n1ed_files);
+        }
+
+        $n1ed_tmp = JPATH_ROOT . '/images/n1ed_tmp';
+
+        if (!JFolder::exists($n1ed_tmp)) {
+            JFolder::create($n1ed_tmp);
+        }
+
+        // Set N1ED as default editor for site
+        $config_file = JPATH_CONFIGURATION . '/configuration.php';
+
+        $current_config = & JFactory::getConfig();
+        $current_config_data = $current_config->toArray();
+
+        $config = new JRegistry('config');
+        $config->loadArray($current_config_data);
+        $config->set('editor', 'n1ed');
+
+        chmod($config_file, 644);
+
+        JFile::write(
+            $config_file, $config->toString('PHP', array('class' => 'JConfig')
+        ));
+
+        chmod($config_file, 444);
     }
 
-    $n1ed_tmp = JPATH_ROOT . '/images/n1ed_tmp';
-
-    if(!JFolder::exists($n1ed_tmp)) {
-      JFolder::create($n1ed_tmp);
-    }
-
-    // Set N1ED as default editor for site
-
-    $config_file = JPATH_CONFIGURATION . '/configuration.php';
-
-    $current_config =& JFactory::getConfig();
-    $current_config_data = $current_config->toArray();
-
-    $config = new JRegistry('config');
-    $config->loadArray($current_config_data);
-    $config->set('editor', 'n1ed');
-
-    chmod($config_file, 644);
-
-    JFile::write(
-      $config_file, 
-      $config->toString('PHP', array('class' => 'JConfig')
-    ));
-
-    chmod($config_file, 444);
-  }
 }
